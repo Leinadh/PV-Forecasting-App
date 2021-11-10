1174b50295275c5302aa4df78c30c1446b0a9eac
Leinadh <daniel.hir2@gmail.com>
2021-11-10 12:43:26 -0500

    add select component

--- src/components/PvfContent.vue
+++ src/components/PvfContent.vue
@@ -8,7 +8,15 @@
       </v-row>
       <v-row cols="12" class="mb-4">
         <v-col>
-          <v-select :items="items" label="Ubicación seleccionada"></v-select>
+          <!-- <PvfSelect /> -->
+          <v-select
+            v-on:change="cambioUbicacion"
+            v-model="ubicacionDefault"
+            v-bind:items="ubicaciones"
+            item-text="texto_ubicacion"
+            item-value="id_ubicacion_modelo"
+            label="Ubicación seleccionada"
+          ></v-select>
           <v-img
             :src="require('../assets/logo.svg')"
             class="my-3"
@@ -17,9 +25,19 @@
           />
 
           <v-card>
-            <v-card-title>Forecasting Lima</v-card-title>
+            <v-card-title>
+              {{
+                filtrarUbicaion(ubicaciones, idUbicacion).length > 0
+                  ? obtenerTitulo(filtrarUbicaion(ubicaciones, idUbicacion)[0])
+                  : ""
+              }}
+            </v-card-title>
             <v-card-text class="text-left">
-              Este modelo fue entrenado en Lima
+              {{
+                filtrarUbicaion(ubicaciones, idUbicacion).length > 0
+                  ? obtenerDescripcion(filtrarUbicaion(ubicaciones, idUbicacion)[0])
+                  : ""
+              }}
             </v-card-text>
           </v-card>
         </v-col>
@@ -79,11 +97,84 @@
 </template>
 
 <script>
+// import PvfSelect from "@/components/PvfSelect";
+
 export default {
   name: "PvfContent",
-
+  components: {
+    // PvfSelect,
+  },
   data: () => ({
-    //
+    ubicacionDefault: { id_ubicacion_modelo: 0, texto_ubicacion: "" },
+    ubicaciones: [
+      {
+        id_ubicacion_modelo: 1,
+        texto_ubicacion: "PUCP - Lima, Lima (PERC)",
+        model_name: "1D-CNN",
+        description: "Descrip...",
+        image: null,
+        is_trasfered: 0,
+        origin_system: null,
+        technology: "PERC",
+        label: "PUCP",
+        full_name: "Pontificia Universidad Católica del Perú",
+        region: "Lima",
+        city: "Lima",
+      },
+      {
+        id_ubicacion_modelo: 2,
+        texto_ubicacion: "UNI - Lima, Lima (PERC)",
+        model_name: "1D-CNN",
+        description: "Descrip...",
+        image: null,
+        is_trasfered: 0,
+        origin_system: null,
+        technology: "PERC",
+        label: "UNI",
+        full_name: "Universidad Nacional de Ingeniería",
+        region: "Lima",
+        city: "Lima",
+      },
+      {
+        id_ubicacion_modelo: 3,
+        texto_ubicacion: "UNI - Lima, Lima (HIT)",
+        model_name: "1D-CNN",
+        description: "Descrip...",
+        image: null,
+        is_trasfered: 0,
+        origin_system: null,
+        technology: "HIT",
+        label: "UNI",
+        full_name: "Universidad Nacional de Ingeniería",
+        region: "Lima",
+        city: "Lima",
+      },
+    ],
+    idUbicacion: 0,
   }),
+  methods: {
+    cambioUbicacion(ubicacion_seleccionada) {
+      this.idUbicacion = ubicacion_seleccionada;
+    },
+    obtenerTitulo(ubicacion_seleccionada) {
+      let transferido_texto = ubicacion_seleccionada.is_trasfered ? ' (Transferido)' : ' (No transferido)'
+      return  'Modelo ' + ubicacion_seleccionada.model_name + transferido_texto;
+    },
+    obtenerDescripcion(ubicacion_seleccionada) {
+      return ubicacion_seleccionada.description;
+    },
+    filtrarUbicaion(ubicaciones, ubicacion_id) {
+      return ubicaciones.filter(
+        (ubicacion) => ubicacion.id_ubicacion_modelo === ubicacion_id
+      );
+    },
+  },
+  mounted: function () {
+    // this.cambioUbicacion("1");\
+
+    //inicializar ubicaciones
+    this.ubicacionDefault = this.ubicaciones[0];
+    this.idUbicacion = 1;
+  },
 };
 </script>
